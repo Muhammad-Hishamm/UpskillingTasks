fec05fa153f42cc845b5f36c46af4f18d2e31fb0
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-09-04 22:41:23 +0300

    feat: Adding the Third part of the first task

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Mileage
+        {
+            get => this.Mileage;
+            set
+            {
+                if (this.Mileage < 0) this.Mileage = 0;
+                else this.Mileage = value;
+            }
+        }
+        public int Age { get => DateTime.Now.Year - this.Year; }
+        public void drive(int distance)
+        {
+            if (distance < 0)
+                throw new ArgumentOutOfRangeException("the distance should be positive");
+            this.Mileage += distance;
+        }
+    }
+
+}
