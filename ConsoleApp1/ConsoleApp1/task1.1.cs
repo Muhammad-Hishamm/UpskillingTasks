dd3254d4fcb9c8559792de79ddbadd4f668c6643
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-09-04 20:54:50 +0300

    feat: Adding the irst task1.1

---
+++ ConsoleApp1/ConsoleApp1/task1.1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        public float Balance { get; private set; }
+
+        public BankAccount(int accountNumber, string accountHolderName = "", float balance = 0)
+        {
+            AccountNumber = accountNumber;
+            AccountHolderName = accountHolderName;
+            if (balance < 0) throw new ArgumentException("Account number should be a positive number");
+            Balance = balance;
+        }
+
+        public bool Deposite(float amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("the deposite amount should be positive");
+                return false;
+            }
+
+            this.Balance += amount;
+            return true;
+        }
+        public bool Withdraw(float amount)
+        {
+            if (this.Balance - amount >= 0)
+            {
+                this.Balance -= amount;
+                return true;
+            }
+            else
+            {
+                throw new ArgumentException("you have no enough Balance");
+                return false;
+            }
+        }
+    }
+}
