e0a99f4b9f6d3b5674289cd57f54453ecc5d62e9
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-09-04 22:47:16 +0300

    feat:adding the project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Price = -100;
+        }
+    }
+}
