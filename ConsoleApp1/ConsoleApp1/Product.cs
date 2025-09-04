1a76a72c5cfa26057d72ced2c7da0cd2b663b955
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-09-04 22:11:30 +0300

    feat: Adding the Second part of the first task

---
+++ ConsoleApp1/ConsoleApp1/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public decimal Price
+        {
+            get => this.Price;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("The price can't be negative.");
+
+                this.Price = value;
+            }
+        }
+
+        public int StockQuantity{ 
+            get=>this.StockQuantity;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("the quantity can't be negative");
+                }
+                this.StockQuantity = value;
+            }
+        }
+
+        public bool IsAvailable
+        {
+            get=>  this.StockQuantity > 0;
+        }
+
+        public void UpdateStock(int amount)
+        {
+            int newQuantity = StockQuantity + amount;
+
+            if (newQuantity < 0)
+                throw new ArgumentException("Cannot reduce stock below zero.");
+
+            StockQuantity = newQuantity;
+        }
+
+    }
+}
