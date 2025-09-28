673486730cdcaead2fcd5ae0f9c5d2bbc6a1ed25
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-09-29 01:27:30 +0300

    feat: Adding Task#5

---
+++ Tasks/Task5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task1.1
+            //List<int> numbers = new List<int> { 3, 9, 2, 7, 10, 15 };
+            //List<int> results = numbers.FindAll((element)=>element>5);
+            //foreach (int i in results)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Task2
+            //List<string> names = new List<string> { "Mona", "Ziad", "Ali", "Laila" };
+            //names.Sort((str1, str2) => str1.Length.CompareTo(str2.Length));
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //task3
+            //Func<int, int, int> addTwo = (x, y) => x + y;
+            //Console.WriteLine(addTwo(10,20));
+
+            //task4
+            //Action<string> great = (str) => Console.WriteLine($"Hello {str}");
+            //great("upskilling");
+        }
+    }
+}
