7407f08a27e955326b4bbdbaf67819dc43d4295c
Muhammad Hisham <72977798+Muhammad-Hishamm@users.noreply.github.com>
2025-10-08 20:00:14 +0300

    feat: Adding the sixth Task (Threading Task)

---
+++ Tasks/Task6/ProducerConsumer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class ProducerConsumer
+{
+    static Queue<int> buffer = new Queue<int>();
+    static int bufferSize = 5;
+    static object lockObject = new object();
+
+    public  static void main()
+    {
+        Thread producer1 = new Thread(Produce);
+        Thread producer2 = new Thread(Produce);
+        Thread consumer1 = new Thread(Consume);
+        Thread consumer2 = new Thread(Consume);
+
+        producer1.Start();
+        producer2.Start();
+        consumer1.Start();
+        consumer2.Start();
+    }
+
+    static void Produce()
+    {
+        while (true)
+        {
+            lock (lockObject)
+            {
+                while (buffer.Count >= bufferSize)
+                {
+                    Monitor.Wait(lockObject);// to avoid deadlock while waiting
+                }
+                int item = new Random().Next(100); 
+                buffer.Enqueue(item);
+                Console.WriteLine($"Produced: {item}");
+                Monitor.PulseAll(lockObject);
+            }
+            Thread.Sleep(new Random().Next(500)); 
+
+        }
+    }
+
+    static void Consume()
+    {
+        while (true)
+        {
+            int item;
+            lock (lockObject)
+            {
+                while (buffer.Count == 0)
+                {
+                    Monitor.Wait(lockObject);
+                }
+                item = buffer.Dequeue();
+                Console.WriteLine($"    Consumed: {item}");
+                Monitor.PulseAll(lockObject);
+            }
+            Thread.Sleep(new Random().Next(1000));
+        }
+    }
+}
+    
---
+++ Tasks/Task6/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        //public static int searchString(string str,ref bool found)
+        //{
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        if(found) return -1; // If already found in another thread, exit early
+        //        if (i < str.Length - 1 && str[i] == 'T' && str[i + 1] == 'o')
+        //        {
+        //            found = true;
+        //            return i;
+        //        }
+        //    }
+        //    return -1;
+        //}
+
+        static void Main(string[] args)
+        {
+
+            //Task 6.1
+            //List<string> lines = new List<string>{ 
+            //                                       "Learning To Code Is Super Fun Now!",
+            //                                       "Programming Skills Grow  Daily!",
+            //                                       "Students Love Creative To Projects"
+            //                                     };
+
+            //bool found = false;
+            //Thread[] threads = new Thread[lines.Count];
+
+            //for (int i = 0; i < lines.Count; i++)
+            //{
+            //    int index = i; 
+            //    threads[i] = new Thread(() =>
+            //    {
+            //        if (searchString(lines[index],ref found)!=-1)
+            //        {
+            //            Console.WriteLine($"Line {index + 1}: {lines[index]}");
+            //        }
+            //    });
+            //    //threads[i].IsBackground = true;
+            //    threads[i].Start();
+            //}
+
+
+            ProducerConsumer.main();
+        }
+    }
+}
